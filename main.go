354d5d97c75eac6a614749c691e14d5d36e9d28e
HirokiHasegawascsk <hi.hasegawa@scsk.jp>
2023-03-28 06:47:34 +0000

    fix

--- main.go
+++ main.go
@@ -1,42 +1,34 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
-// ResponseData はAPIからのレスポンスデータを表す構造体です。
-type ResponseData struct {
-	Message string `json:"message"`
-}
-
 func main() {
-	apiURL := "https://go-codescanning-githubactions-cloudrun-wsgwmfbvhq-uc.a.run.app/"
-	response, err := callAPI(apiURL)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
 	}
-	fmt.Println("結果:", response)
+
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func callAPI(url string) (string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return "", err
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "World"
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	var data ResponseData
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		return "", err
-	}
-	return data.Message + "接続できた", nil
+	fmt.Fprintf(w, "Hello %s!\n", name)
 }
