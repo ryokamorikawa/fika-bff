ea81c0ec4deef2b68df2673877d1d169793931aa
HirokiHasegawascsk <hi.hasegawa@scsk.jp>
2023-03-30 01:59:58 +0000

    fix

--- main.go
+++ main.go
@@ -9,27 +9,7 @@ import (
 	"os"
 )
 
-// ResponseData はAPIからのレスポンスデータを表す構造体です。
-type ResponseData struct {
-	Message string `json:"message"`
-}
-
 func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
 	apiURL := "https://go-codescanning-githubactions-cloudrun-wsgwmfbvhq-uc.a.run.app/"
 	/*接続を試みる*/
 	conn, err := net.Dial("tcp", apiURL)
@@ -44,13 +24,16 @@ func main() {
 	buff := make([]byte, 2048) //ある程度のサイズ
 	res, err := bufio.NewReader(conn).Read(buff)
 	fmt.Printf("%s", buff[:res])
-
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "BFF"
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
 	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
+
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
