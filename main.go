1b7e61bc2423641671b674190077ddc7edbdaf17
HirokiHasegawascsk <hi.hasegawa@scsk.jp>
2023-03-30 02:39:34 +0000

    fix

--- main.go
+++ main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	apiURL := "https://go-codescanning-githubactions-cloudrun-wsgwmfbvhq-uc.a.run.app/"
-	res, _ := http.Get(apiURL)
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
 
-	fmt.Println(res.StatusCode) // HTTPレスポンスステータス
-	fmt.Println(res.Proto)      // HTTPプロトコル
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!\n", name)
 }
