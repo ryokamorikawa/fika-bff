eeee641c7384ae6713129c90986bcfd684079a4d
HirokiHasegawascsk <hi.hasegawa@scsk.jp>
2023-03-30 02:36:29 +0000

    fix

--- main.go
+++ main.go
@@ -9,10 +9,6 @@ func main() {
 	apiURL := "https://go-codescanning-githubactions-cloudrun-wsgwmfbvhq-uc.a.run.app/"
 	res, _ := http.Get(apiURL)
 
-	fmt.Println(res.StatusCode)             // HTTPレスポンスステータス
-	fmt.Println(res.Proto)                  // HTTPプロトコル
-	fmt.Println(res.Header["Date"])         // データを取得した日付と時間
-	fmt.Println(res.Header["Content-Type"]) // コンテンツのタイプ
-	fmt.Println(res.Request.Method)         // GETかPOST
-	fmt.Println(res.Request.URL)            // URL
+	fmt.Println(res.StatusCode) // HTTPレスポンスステータス
+	fmt.Println(res.Proto)      // HTTPプロトコル
 }
