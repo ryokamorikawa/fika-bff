5c06bb6cd821b77083819f5d239361cbd82a1d4c
HirokiHasegawascsk <hi.hasegawa@scsk.jp>
2023-03-30 03:47:03 +0000

    fix

--- main.go
+++ main.go
@@ -16,6 +16,11 @@ func main() {
 		port = "8080"
 		log.Printf("defaulting to port %s", port)
 	}
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 
 }
 
