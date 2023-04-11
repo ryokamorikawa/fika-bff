e83f425ba88fe6ca91b9a8131eb7224289105fd8
scsk-morikawa <r.morikawa@scsk.jp>
2023-04-11 18:24:05 +0900

    cloud run間の認証が通るように実装

--- main.go
+++ main.go
@@ -15,14 +15,6 @@ import (
 const Api1Url = "https://cloudrun-api1-morikawa-test-wsgwmfbvhq-uc.a.run.app"
 const Api2Url = "https://cloudrun-api2-morikawa-test-wsgwmfbvhq-uc.a.run.app"
 
-// 実際は以下構造体はどこかで共通化する想定
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
 func main() {
 	router := gin.Default()
 
@@ -46,7 +38,7 @@ func main() {
 func getAlbums(c *gin.Context) {
 	ctx := context.Background()
 
-	client, err := idtoken.NewClient(ctx, "https://cloudrun-api1-morikawa-test-wsgwmfbvhq-uc.a.run.app")
+	client, err := idtoken.NewClient(ctx, Api1Url)
 	if err != nil {
 		fmt.Printf("idtoken.NewClient: %v\n", err)
 		c.JSON(http.StatusInternalServerError, err)
@@ -69,59 +61,85 @@ func getAlbums(c *gin.Context) {
 
 }
 
-func getUsers(c *gin.Context) {
-	res, _ := http.Get(Api2Url + "/users")
-	defer res.Body.Close()
-
-	// 取得したURLの内容を読み込む
-	body, _ := io.ReadAll(res.Body)
-	log.Println(string(body))
-
-	c.JSON(res.StatusCode, string(body))
-}
-
-// func getAlbums(c *gin.Context) {
-// 	res, _ := http.Get(Api1Url + "/albums")
-// 	defer res.Body.Close()
-
-// 	// 取得したURLの内容を読み込む
-// 	body, _ := io.ReadAll(res.Body)
-// 	log.Println(string(body))
-
-// 	c.JSON(res.StatusCode, string(body))
-// }
-
 func getAlbumByID(c *gin.Context) {
+
 	id := c.Param("id")
-	res, _ := http.Get(Api1Url + "/albums/" + string(id))
-	defer res.Body.Close()
+	ctx := context.Background()
+
+	client, err := idtoken.NewClient(ctx, Api1Url)
+	if err != nil {
+		fmt.Printf("idtoken.NewClient: %v\n", err)
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	resp, err := client.Get(Api1Url + "/albums/" + string(id))
+	if err != nil {
+		fmt.Printf("client.Get: %v\n", err)
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	defer resp.Body.Close()
 
 	// 取得したURLの内容を読み込む
-	body, _ := io.ReadAll(res.Body)
+	body, _ := io.ReadAll(resp.Body)
 	log.Println(string(body))
 
-	c.JSON(res.StatusCode, string(body))
+	c.JSON(resp.StatusCode, string(body))
+
 }
 
-// func getUsers(c *gin.Context) {
-// 	res, _ := http.Get(Api2Url + "/users")
-// 	defer res.Body.Close()
+func getUsers(c *gin.Context) {
 
-// 	// 取得したURLの内容を読み込む
-// 	body, _ := io.ReadAll(res.Body)
-// 	log.Println(string(body))
+	ctx := context.Background()
 
-// 	c.JSON(res.StatusCode, string(body))
-// }
+	client, err := idtoken.NewClient(ctx, Api2Url)
+	if err != nil {
+		fmt.Printf("idtoken.NewClient: %v\n", err)
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	resp, err := client.Get(Api2Url + "/users")
+	if err != nil {
+		fmt.Printf("client.Get: %v\n", err)
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// 取得したURLの内容を読み込む
+	body, _ := io.ReadAll(resp.Body)
+	log.Println(string(body))
+
+	c.JSON(resp.StatusCode, string(body))
+
+}
 
 func getUserByID(c *gin.Context) {
+
 	id := c.Param("id")
-	res, _ := http.Get(Api2Url + "/users/" + string(id))
-	defer res.Body.Close()
+	ctx := context.Background()
+
+	client, err := idtoken.NewClient(ctx, Api2Url)
+	if err != nil {
+		fmt.Printf("idtoken.NewClient: %v\n", err)
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	resp, err := client.Get(Api2Url + "/users/" + string(id))
+	if err != nil {
+		fmt.Printf("client.Get: %v\n", err)
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+	defer resp.Body.Close()
 
 	// 取得したURLの内容を読み込む
-	body, _ := io.ReadAll(res.Body)
+	body, _ := io.ReadAll(resp.Body)
 	log.Println(string(body))
 
-	c.JSON(res.StatusCode, string(body))
+	c.JSON(resp.StatusCode, string(body))
+
 }
