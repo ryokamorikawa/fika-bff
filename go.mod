370951f2bcb68bc9703ad845c96b66147cc67494
scsk-morikawa <r.morikawa@scsk.jp>
2023-04-11 10:43:56 +0900

    api1とapi2を呼び出すように実装

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/GoogleCloudPlatform/golang-samples/run/helloworld
- 
+
 go 1.13
+
+require github.com/gin-gonic/gin v1.9.0
--- main.go
+++ main.go
@@ -1,63 +1,89 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
+const Api1Url = "https://cloudrun-api1-morikawa-test-wsgwmfbvhq-uc.a.run.app"
+const Api2Url = "https://cloudrun-api2-morikawa-test-wsgwmfbvhq-uc.a.run.app"
 
-	// Determine port for HTTP service.
+// 実際は以下構造体はどこかで共通化する想定
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	// router.POST("/albums", postAlbums)
+
+	router.GET("/users", getUsers)
+	router.GET("/users/:id", getUserByID)
+	// router.POST("/users", postUsers)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 		log.Printf("defaulting to port %s", port)
 	}
 
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
+	router.Run("0.0.0.0:" + port)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	// name := os.Getenv("NAME")
-	// if name == "" {
-	// 	name = "bff"
-	// }
-	// res1, _ := http.Get("https://go-codescanning-githubactions-cloudrun-api1-wsgwmfbvhq-uc.a.run.app")
-	// // 取得したURLの内容を読み込む
-	// body1, _ := io.ReadAll(res1.Body)
-	// // 取得した情報は[]byteなのでstringに型変換
-	// fmt.Fprintf(w, "Body %s\n", string(body1))
-	// fmt.Fprintf(w, "Status %s\n", res1.StatusCode)
-	// fmt.Fprintf(w, "URL %s\n", res1.Request.URL)
-	// fmt.Fprintf(w, "Protocol %s\n", res1.Proto)
-	// fmt.Fprintf(w, "Date %s\n", res1.Header["Date"])
-	// fmt.Fprintf(w, "Content-Type %s\n", res1.Header["Content-Type"])
-	// fmt.Fprintf(w, "Method %s\n", res1.Request.Method)
+// TODO: errハンドリング
+func getAlbums(c *gin.Context) {
+	res, _ := http.Get(Api1Url + "/albums")
+	defer res.Body.Close()
 
-	// res2, _ := http.Get("https://go-codescanning-githubactions-cloudrun-api2-wsgwmfbvhq-uc.a.run.app")
-	// // 取得したURLの内容を読み込む
-	// body2, _ := io.ReadAll(res2.Body)
-	// // 取得した情報は[]byteなのでstringに型変換
-	// fmt.Fprintf(w, "Body %s\n", string(body2))
-	// fmt.Fprintf(w, "Status %s\n", res2.StatusCode)
-	// fmt.Fprintf(w, "URL %s\n", res2.Request.URL)
-	// fmt.Fprintf(w, "Protocol %s\n", res2.Proto)
-	// fmt.Fprintf(w, "Date %s\n", res2.Header["Date"])
-	// fmt.Fprintf(w, "Content-Type %s\n", res2.Header["Content-Type"])
-	// fmt.Fprintf(w, "Method %s\n", res2.Request.Method)
+	// 取得したURLの内容を読み込む
+	body, _ := io.ReadAll(res.Body)
+	log.Println(string(body))
 
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "BFF"
-	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
+	c.IndentedJSON(http.StatusOK, string(body))
+}
 
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+	res, _ := http.Get(Api1Url + "/albums/" + string(id))
+	defer res.Body.Close()
+
+	// 取得したURLの内容を読み込む
+	body, _ := io.ReadAll(res.Body)
+	log.Println(string(body))
+
+	c.IndentedJSON(http.StatusOK, string(body))
+}
+
+
+func getUsers(c *gin.Context) {
+	res, _ := http.Get(Api2Url + "/users")
+	defer res.Body.Close()
+
+	// 取得したURLの内容を読み込む
+	body, _ := io.ReadAll(res.Body)
+	log.Println(string(body))
+
+	c.IndentedJSON(http.StatusOK, string(body))
+}
+
+func getUserByID(c *gin.Context) {
+	id := c.Param("id")
+	res, _ := http.Get(Api2Url + "/users/" + string(id))
+	defer res.Body.Close()
+
+	// 取得したURLの内容を読み込む
+	body, _ := io.ReadAll(res.Body)
+	log.Println(string(body))
+
+	c.IndentedJSON(http.StatusOK, string(body))
 }
